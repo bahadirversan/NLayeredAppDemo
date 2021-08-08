dc15c66f233bc2c434691cf78024846ba37b7899
Bahadır Versan <versan412010@hotmail.com>
2021-08-08 19:57:28 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {        
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Northwind.WebFormsUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.Business.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Northwind.WebFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _productService = new ProductManager(new EfProductDal());
+        }
+
+        private IProductService _productService;
+        private void Form1_Load(object sender, EventArgs e)
+        {        
+            dgwProduct.DataSource = _productService.GetAll();
+        }
+    }
+}
